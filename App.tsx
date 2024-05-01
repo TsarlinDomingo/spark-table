7f5da99652a2d64543b2b444132938e16b7a427c
Tsarlin Marie Domingo <tmb.domingo@gmail.com>
2024-05-01 16:02:16 +0100

    feat: add next button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white space-y-8">
-      <Text className="text-4xl">Welcome to Spark Table!</Text>
+    <View className="flex-1 flex-direction flex-col items-center justify-center bg-white space-y-8 h-full">
+      <Text className="text-4xl">Welcome to Spark Table 👋</Text>
       <Text className="text-xl">Have meaningful and revealing conversations with anyone</Text>
+      <View className="flex-2 flex-row space-x-2 items-center">
+        <TouchableOpacity className="rounded-3xl bg-blue-500 px-10 py-4 h-fit" onPress={() => console.log('next')}>
+          <Text className="text-white font-medium text-xl">
+            Next
+          </Text>
+        </TouchableOpacity>
+        <Text className="text-3xl">
+          ➡️
+        </Text>  
+      </View>
       <StatusBar style="auto" />
     </View>
   );
