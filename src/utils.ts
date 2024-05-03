3c4ca8fddd31f6447c9c7abfdf1ad7965e83784f
Tsarlin Marie Domingo <tmb.domingo@gmail.com>
2024-05-03 20:25:11 +0100

    chore: add docs for util helper getAnotherId;

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,14 @@
+/**
+ * Return a random integer from 1 to 10
+ * other than the number id passed to it
+ * 
+ * @param id number
+ * @returns number
+ */
 export const getAnotherId = (id: number): number => {
   const randomId = Math.floor(Math.random() * 10);
   if (id === randomId) {
     return getAnotherId(id);
   }
   return randomId;
-}
+};
